f271eb548e90c79289dc218aeee50bcf00a3d486
Ionut Margarint <ionutmargarint@gmail.com>
2019-09-21 11:15:48 +0300

    webpack and babel setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => (
+	<div className='app'>
+		<div className='card'>
+			<h1 className='title'>Hello, World!</h1>
+		</div>
+	</div>
+);
+
+export default App;
